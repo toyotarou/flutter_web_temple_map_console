88ae4707cfea7db829a248b304c542d4d722a961
toyodahideyuki <toyodahideyuki4819@gmail.com>
2025-11-27 20:19:44 +0900

    commit 20251127

--- lib/screens/right_screen.dart
+++ lib/screens/right_screen.dart
@@ -5,6 +5,7 @@ import 'package:latlong2/latlong.dart';
 import '../controllers/controllers_mixin.dart';
 import '../extensions/extensions.dart';
 import '../models/common/spot_data_model.dart';
+import '../models/temple_lat_lng_model.dart';
 import '../models/temple_model.dart';
 import '../utility/daily_spot_data_functions.dart';
 
@@ -26,7 +27,7 @@ class _RightScreenState extends ConsumerState<RightScreen> with ControllersMixin
 
   List<Marker> matchedSpotsMarkerList = <Marker>[];
 
-  List<Marker> notMatchedSpotsMarkerList = <Marker>[];
+  List<Marker> templeMarkerList = <Marker>[];
 
   ///
   @override
@@ -57,9 +58,7 @@ class _RightScreenState extends ConsumerState<RightScreen> with ControllersMixin
               children: <Widget>[
                 TileLayer(urlTemplate: 'https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png'),
 
-                if (matchedSpotsMarkerList.isNotEmpty) ...<Widget>[MarkerLayer(markers: matchedSpotsMarkerList)],
-
-                if (notMatchedSpotsMarkerList.isNotEmpty) ...<Widget>[MarkerLayer(markers: notMatchedSpotsMarkerList)],
+                if (templeMarkerList.isNotEmpty) ...<Widget>[MarkerLayer(markers: templeMarkerList)],
               ],
             ),
           ),
@@ -85,63 +84,23 @@ class _RightScreenState extends ConsumerState<RightScreen> with ControllersMixin
 
       final SpotDataModel firstTempleSpotData = spotDataModelList[1];
 
-      mapController.move(LatLng(firstTempleSpotData.latitude.toDouble(), firstTempleSpotData.longitude.toDouble()), 18);
+      mapController.move(LatLng(firstTempleSpotData.latitude.toDouble(), firstTempleSpotData.longitude.toDouble()), 16);
 
       appParamNotifier.setIsMapCenterMove(flag: true);
 
-      makeMatchedSpotsMarkerList();
+      makeTempleMarkerList();
     });
   }
 
   ///
-  void makeMatchedSpotsMarkerList() {
-    for (final SpotDataModel element in spotDataModelList) {
-      matchedSpotsMarkerList.add(
-        Marker(point: LatLng(element.latitude.toDouble(), element.longitude.toDouble()), child: const CircleAvatar()),
+  void makeTempleMarkerList() {
+    getDataState.keepTempleLatLngMap.forEach((String key, TempleLatLngModel value) {
+      templeMarkerList.add(
+        Marker(
+          point: LatLng(value.lat.toDouble(), value.lng.toDouble()),
+          child: const CircleAvatar(backgroundColor: Colors.redAccent),
+        ),
       );
-    }
-
-    makeNotMatchedSpotsMarkerList();
-  }
-
-  ///
-  void makeNotMatchedSpotsMarkerList() {
-    final List<String> list = <String>[];
-    final List<String> list2 = <String>[];
-
-    for (final SpotDataModel element in spotDataModelList) {
-      list.add('${element.latitude}|${element.longitude}');
-
-      list2.add(element.name);
-    }
-
-    getDataState.keepTempleList.where((TempleModel a) => a.date.yyyymmdd != appParamState.selectedDate).forEach((
-      TempleModel element,
-    ) {
-      final Map<String, dynamic> dailySpotDataInfo = getDailySpotDataInfo(
-        templeModel: element,
-        templeLatLngMap: getDataState.keepTempleLatLngMap,
-        stationMap: getDataState.keepStationMap,
-        tokyoMunicipalList: getDataState.keepTokyoMunicipalList,
-        templeListMap: getDataState.keepTempleListMap,
-      );
-
-      final List<SpotDataModel> templeDataList = dailySpotDataInfo['templeDataList'] as List<SpotDataModel>;
-
-      for (final SpotDataModel element2 in templeDataList) {
-        if (!list.contains('${element2.latitude}|${element2.longitude}')) {
-          if (!list2.contains(element2.name)) {
-            if (element2.type == 'temple') {
-              notMatchedSpotsMarkerList.add(
-                Marker(
-                  point: LatLng(element2.latitude.toDouble(), element2.longitude.toDouble()),
-                  child: const CircleAvatar(backgroundColor: Colors.redAccent),
-                ),
-              );
-            }
-          }
-        }
-      }
     });
   }
 }
